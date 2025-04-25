59b3559d5a606aa745566b6eea5c2af1808da929
Tquapol <julesrobin35@gmail.com>
2025-04-25 12:19:45 +0200

    Update main.cpp

--- Sandbox-POO/main.cpp
+++ Sandbox-POO/main.cpp
@@ -1,7 +1,8 @@
 // Jules ROBIN
 
 #include <iostream>
-#include "Scène.h"
+#include <Windows.h>
+#include "Scene.h"
 #include "Pierre.h"
 #include "Sable.h"
 #include "Eau.h"
@@ -9,22 +10,28 @@
 using namespace std;
 
 int main() {
-	Scene scene;
-	scene.print();
-	cout << endl;
+	Scene scene(11, 11);
 
-	scene.setMaterial(new Pierre, 2, 2);
-	scene.setMaterial(new Sable, 1, 2);
-	scene.setMaterial(new Eau, 0, 2);
-	scene.print();
-	cout << endl;
+	for (int i = 0; i < 7; i++) {
+		scene.setMaterial(new Pierre, 9, 2 + i);
+	}
+	for (int i = 0; i < 3; i++) {
+		scene.setMaterial(new Eau, 7, 4 + i);
+	}
+	scene.setMaterial(new Pierre, 8, 2);
+	scene.setMaterial(new Pierre, 8, 8);
 
-	scene.removeMaterial(2, 2);
+	scene.setMaterial(new Sable, 2, 5);
+	scene.setMaterial(new Sable, 1, 5);
+	scene.setMaterial(new Sable, 0, 5);
 	scene.print();
-	cout << endl;
 
-	scene.resize(3, 8);
-	scene.print();
+	for (int i = 0; i < 20; i++) {
+		Sleep(1000);
+		system("CLS");
+		scene.evolve();
+		scene.print();
+	}
 
 	return 0;
 }
