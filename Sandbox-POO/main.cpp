e6ec0d1721e4f3752267b100668fbc86ef07bd8d
Tquapol <julesrobin35@gmail.com>
2025-04-28 11:09:24 +0200

    InerSable + vide
    Ajout de l'InerSable et prise en compte des bordures solides ou vides de la scène.

--- Sandbox-POO/main.cpp
+++ Sandbox-POO/main.cpp
@@ -5,43 +5,29 @@
 #include "Scene.h"
 #include "Pierre.h"
 #include "Sable.h"
+#include "InerSable.h"
 #include "Eau.h"
 
 using namespace std;
 
 int main() {
-	Scene scene(11, 11);
+	srand(time(nullptr));
+	Scene scene(15, 20, false);
 
-	for (int i = 0; i < 7; i++) {
-		scene.setMaterial(new Pierre, 10, 2 + i);
-	}
-	for (int i = 0; i < 3; i++) {
-		scene.setMaterial(new Eau, 7, 4 + i);
-		scene.setMaterial(new Eau, 6, 4 + i);
-	}
-	for (int i = 0; i < 3; i++) {
-		scene.setMaterial(new Eau, 1+i, 6);
-	}
-	scene.setMaterial(new Pierre, 9, 2);
-	scene.setMaterial(new Pierre, 9, 8);
-	scene.setMaterial(new Pierre, 8, 2);
-	scene.setMaterial(new Pierre, 8, 8);
-
-	scene.setMaterial(new Sable, 2, 5);
-	scene.setMaterial(new Sable, 1, 5);
-	scene.setMaterial(new Sable, 0, 5);
-	scene.print();
+	scene.setMaterial(new Pierre, 10, 3);
+	scene.setMaterial(new Pierre, 10, 8);
+	scene.setMaterial(new Pierre, 10, 13);
+	scene.setMaterial(new Pierre, 10, 18);
+	scene.setMaterial(new InerSable, 9, 3);
+	scene.setMaterial(new InerSable, 6, 8);
+	scene.setMaterial(new Sable, 9, 13);
+	scene.setMaterial(new Sable, 6, 18);
 
 	for (int i = 0; i < 30; i++) {
-		Sleep(500);
 		system("CLS");
-		scene.evolve();
 		scene.print();
-		if (i == 10) {
-			scene.removeMaterial(10, 4);
-			scene.removeMaterial(10, 7);
-			scene.resize(20, 11);
-		}
+		scene.evolve();
+		Sleep(500);
 	}
 
 	return 0;
